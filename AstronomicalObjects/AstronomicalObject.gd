f295fc0943f43e4a3eea7023c299cec45a976a39
sunderlain <sunderlain@ggs.ch>
2020-03-31 12:36:46 +0200

    ~minor inspector and solar mass changes

--- AstronomicalObjects/AstronomicalObject.gd
+++ AstronomicalObjects/AstronomicalObject.gd
@@ -3,7 +3,8 @@ extends KinematicBody
 var G = 6.67*pow(10,-11)
 export var direction = Vector3(0,0,0)
 export var active = true
-export var mass = 100000000
+export(float) var solar_mass = 1
+var solar_mass_to_kg = 200000 #2×10~30 would be more realistic, but I prefer to substitute my own reality
 
 func _enter_tree():
     active = true
@@ -12,11 +13,10 @@ func _ready():
     active = true
 
 func get_pulled_towards_object(object, delta):
-    if "mass" in object and "translation" in object:
+    if "solar_mass" in object and "translation" in object:
         var distanceToObject = translation.distance_to(object.translation)
-        if object.mass>0 and distanceToObject>0:
-            var gravitationalPull = sqrt(G*(object.mass/distanceToObject))
-            
+        if object.solar_mass>0 and distanceToObject>0:
+            var gravitationalPull = sqrt(G*(object.solar_mass*solar_mass_to_kg/distanceToObject))
             var gravityDirection = (object.translation-translation).normalized()
             direction+=gravityDirection*gravitationalPull
             
--- AstronomicalObjects/Star.gd
+++ AstronomicalObjects/Star.gd
@@ -1,9 +1,6 @@
+tool
 extends "res://AstronomicalObjects/AstronomicalObject.gd"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass
     
 func _process(delta):
     rotate_object_local(Vector3(0, 1, 0), PI * delta / 60)
