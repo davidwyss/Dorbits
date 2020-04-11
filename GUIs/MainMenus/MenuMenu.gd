14bd550c17d5fac253b6a3994492b38d92285783
david.wyss <david.wyss@ggs.ch>
2020-04-11 16:28:28 +0200

    Minor Setting changes

--- GUIs/MainMenus/MenuMenu.gd
+++ GUIs/MainMenus/MenuMenu.gd
@@ -13,12 +13,15 @@ func _process(_delta):
         if Input.is_action_just_pressed("ui_up"):
             select_menu_option(selected_label_index - 1 if  selected_label_index - 1 >= 0 else labelcount -1)
         if Input.is_action_just_pressed("ui_right") || Input.is_action_pressed("enter"):
-            if selected_label_index == 2:
-                # warning-ignore:return_value_discarded
-                get_tree().change_scene("res://GUIs/SatelliteInfos/SatelliteInfo.tscn")
-            else:    
+            if selected_label_index == 0:
                 # warning-ignore:return_value_discarded
                 get_tree().change_scene("res://Main.tscn")
+            elif selected_label_index == 1:
+                # warning-ignore:return_value_discarded
+                get_tree().change_scene("res://GUIs/SatelliteInfos/SatelliteInfo.tscn")
+            elif selected_label_index == 2:    
+                # warning-ignore:return_value_discarded
+                get_tree().change_scene("res://GUIs/other/IWANTTOFREEYOURSKIN.tscn")
             
 
 func select_menu_option(index):
--- GUIs/other/NOMOREMISTERNICEGODOT.shader
+++ GUIs/other/NOMOREMISTERNICEGODOT.shader
@@ -2,7 +2,7 @@ shader_type canvas_item;
 
 
 const float PI = 3.14159265358979323846;
-uniform float HP_PERCENT = 1.;
+uniform float SPEED = 1.;
 
 
 vec2 rotateUV(vec2 uv, vec2 pivot, float rotation) {
@@ -19,13 +19,12 @@ vec2 rotateUV(vec2 uv, vec2 pivot, float rotation) {
 
 
 void fragment() {
-    float speed=0.2;
     vec2 uv = UV;
-    uv /= sin(TIME*speed);
-    uv += -abs(cos(TIME*speed)*0.25);
-    uv = rotateUV(uv,vec2(.5),3.141*sin(TIME*speed));
+    uv /= abs(sin(TIME*SPEED));
+    uv += -abs(cos(TIME*SPEED)*0.25);
+    uv = rotateUV(uv,vec2(.5),3.141*sin(TIME*SPEED));
     vec2 center = vec2(0.5);
-    float max_intensity_range = fract(TIME*speed);
+    float max_intensity_range = fract(TIME*SPEED);
     float dist = distance(uv,center); 
     COLOR.rgb = texture(TEXTURE, uv).rgb;
     COLOR.rgb += vec3(dist);
