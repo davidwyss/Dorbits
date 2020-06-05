7296ab0fd1596ea3ba7d6c9af0567075aeca6343
david.wyss <david.wyss@ggs.ch>
2020-06-05 21:54:13 +0200

    +more solar info +modified subthrusters

--- GUIs/SpaceObjectInfos/StarInfos/StarInfo.gd
+++ GUIs/SpaceObjectInfos/StarInfos/StarInfo.gd
@@ -4,7 +4,10 @@ var star
 
 func set_star(_star):
     star = _star
-    for prop in ["Name: %s" % star.name,"Surface_Kelvin: %s" %  star.solar_surface_kelvin,"Radius: %s" %  star.solar_radius]:
+    for prop in ["Name: %s" % star.name,
+                "Surface Kelvin: %s" %  star.solar_surface_kelvin,
+                "Radius: %s" % star.solar_radius,
+                "Solar Mass %s" % star.solar_mass]:
         var nameProp = Label.new()
         nameProp.text = prop
         $StarInfoList.add_child(nameProp)
