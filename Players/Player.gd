4f4e674fc95cda232df63a573d9249d335f7c80f
david.wyss <david.wyss@ggs.ch>
2020-06-05 21:52:01 +0200

    Used a more appropriate term for this highly professional project

--- Players/Player.gd
+++ Players/Player.gd
@@ -1,6 +1,11 @@
 extends Node
 
+var id
 var rift
+var nickname
+var is_puppet
 
-func init(nickname, is_slave):
+func init(_nickname, _is_puppet):
     $Name.text = nickname
+    nickname = _nickname
+    is_puppet = _is_puppet
